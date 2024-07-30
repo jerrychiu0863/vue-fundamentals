f4010d10511cbc9cc8d326c7b0eb90239e464adf
Jerry Chiu <m08632000@hotmail.com>
2024-07-30 14:57:52 +0800

    add addtask and deletetask functionality

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,4 @@
 <!-- <script>
-// Composition api longer version
-import { ref } from "vue";
-export default {
-  setup() {
-    const name = ref("John");
-    const status = ref("active");
-    const tasks = ref([
-      { id: 1, title: "Task one" },
-      { id: 2, title: "Task two" },
-      { id: 3, title: "Task three" },
-    ]);
-
-    const toggleStatus = () => {
-      console.log(status);
-      if (status.value === "active") {
-        status.value = "pending";
-        return;
-      }
-      if (status.value === "pending") {
-        status.value = "inactive";
-        return;
-      }
-      if (status.value === "inactive") {
-        status.value = "active";
-        return;
-      }
-    };
-
-    return { name, status, tasks, toggleStatus };
-  },
-};
-</script> -->
-<script setup>
-// Composition api shortor version
-import { ref } from "vue";
-
-const name = ref("John");
-const status = ref("active");
-const tasks = ref([
-  { id: 1, title: "Task one" },
-  { id: 2, title: "Task two" },
-  { id: 3, title: "Task three" },
-]);
-
-const toggleStatus = () => {
-  console.log(status);
-  if (status.value === "active") {
-    status.value = "pending";
-    return;
-  }
-  if (status.value === "pending") {
-    status.value = "inactive";
-    return;
-  }
-  if (status.value === "inactive") {
-    status.value = "active";
-    return;
-  }
-};
-</script>
-<!-- <script>
 // Options API
 // export default {
 //   data() {
@@ -104,6 +43,79 @@ const toggleStatus = () => {
 //   // },
 // };
 </script> -->
+<!-- <script>
+// Composition api longer version
+import { ref } from "vue";
+export default {
+  setup() {
+    const name = ref("John");
+    const status = ref("active");
+    const tasks = ref([
+      { id: 1, title: "Task one" },
+      { id: 2, title: "Task two" },
+      { id: 3, title: "Task three" },
+    ]);
+
+    const toggleStatus = () => {
+      console.log(status);
+      if (status.value === "active") {
+        status.value = "pending";
+        return;
+      }
+      if (status.value === "pending") {
+        status.value = "inactive";
+        return;
+      }
+      if (status.value === "inactive") {
+        status.value = "active";
+        return;
+      }
+    };
+
+    return { name, status, tasks, toggleStatus };
+  },
+};
+</script> -->
+<script setup>
+// Composition api shortor version
+import { ref } from "vue";
+
+const name = ref("John");
+const status = ref("active");
+const tasks = ref([
+  { id: 1, title: "Task one" },
+  { id: 2, title: "Task two" },
+  { id: 3, title: "Task three" },
+]);
+const newTask = ref("");
+
+const toggleStatus = () => {
+  console.log(tasks);
+  if (status.value === "active") {
+    status.value = "pending";
+    return;
+  }
+  if (status.value === "pending") {
+    status.value = "inactive";
+    return;
+  }
+  if (status.value === "inactive") {
+    status.value = "active";
+    return;
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim()) {
+    tasks.value.push({ id: Math.random(), title: newTask.value });
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (id) => {
+  tasks.value = tasks.value.filter((task) => task.id !== id);
+};
+</script>
 
 <template>
   <h1>{{ name }}</h1>
@@ -116,9 +128,19 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks</h3>
   <ul>
-    <li v-for="task in tasks" :key="task.id">{{ task.title }}</li>
+    <li v-for="(task, index) in tasks" :key="task.id">
+      <span>Index: {{ index }}</span>
+      <span>{{ task.title }}</span>
+      <button @click="deleteTask(task.id)">Delete</button>
+    </li>
   </ul>
 
   <!-- <a v-bind:href="link">Google</a> -->
